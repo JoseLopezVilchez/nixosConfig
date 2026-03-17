308a5d84493114db666f7bc5cbcf883002de458f
Jose <joselopezvilchez.dev@gmail.com>
2026-03-17 23:48:37 +0100

    Trying to fix errors

--- hosts/home.nix
+++ hosts/home.nix
@@ -5,55 +5,53 @@
     ../modules/homeManager/ai-services.nix
   ];
 
-  home = {
-    username = "redfinger";
-    homeDirectory = "/home/redfinger";
+  home.username = "redfinger";
+  home.homeDirectory = "/home/redfinger";
 
-    # This value determines the Home Manager release that your configuration is
-    # compatible with. This helps avoid breakage when a new Home Manager release
-    # introduces backwards incompatible changes.
-    #
-    # You should not change this value, even if you update Home Manager. If you do
-    # want to update the value, then make sure to first check the Home Manager
-    # release notes.
-    stateVersion = "25.11"; # Please read the comment before changing.
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "25.11"; # Please read the comment before changing.
 
-    packages = with pkgs; [
-      discord
-      rustc
-      cargo
-      tor-browser
-      gcc
-      gnumake
-      zed-editor
-      rust-analyzer
-      nixd
-      nil
-      prismlauncher
+  home.packages = with pkgs; [
+    discord
+    rustc
+    cargo
+    tor-browser
+    gcc
+    gnumake
+    zed-editor
+    rust-analyzer
+    nixd
+    nil
+    prismlauncher
 
-      # # You can also create simple shell scripts directly inside your
-      # # configuration. For example, this adds a command 'my-hello' to your
-      # # environment:
-      # (pkgs.writeShellScriptBin "my-hello" ''
-      #   echo "Hello, ${config.home.username}!"
-      # '')
-    ];
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
 
-    file = {
+  home.file = {
 
-    };
+  };
 
-    # Home Manager can also manage your environment variables
-    sessionVariables = {
+  # Home Manager can also manage your environment variables
+  home.sessionVariables = {
 
-    };
+  };
 
-    programs = {
-      git = {
-        enable = true;
-        userName = "José López Vílchez";
-        userEmail = "joselopezvilchez.dev@gmail.com";
-      };
+  home.programs = {
+    git = {
+      enable = true;
+      userName = "José López Vílchez";
+      userEmail = "joselopezvilchez.dev@gmail.com";
     };
   };
 
